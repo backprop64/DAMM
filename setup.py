b63aa7e0a0a5336f39b9c9a911a3b74ac74f19f3
Gaurav Kaul <59609507+backprop64@users.noreply.github.com>
2024-01-13 15:06:25 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('requirements-collab.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='DAMM',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=required,
+    author='Gaurav Kaul',
+    author_email='kaulg@umich.com',
+)
